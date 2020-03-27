5e9bd4213bcebad5d04768dc1a4d9d20c1d18951
AuFish <shenchg126@gmail.com>
2020-03-27 13:08:09 -0700

    training function

--- BERT/run_squad.sh
+++
@@ -1,6 +0,0 @@
-BERT_BASE_DIR="/home/aufish/Downloads/bert"
-
-python3 -m bert.run_squad\
-    --vocab_file=$BERT_BASE_DIR/assets/vocab.txt \
-    --init_checkpoint=$BERT_BASE_DIR/saved_model.pb\
-    --output_dir=/tmp/squad_base/
